c9862440ab7a8fff9d386cae10d5a1af74b190b6
Purag Moumdjian <purmou@gmail.com>
2014-03-02 17:03:40 -0800

    Create globalFunctions.php
    Some simple functions to modify content returned from global.php

---
+++ globalFunctions.php
@@ -0,0 +1,38 @@
+<?php
+    //truncate text
+	function truncate($string, $limit, $break=".", $pad=". ") {
+		// return with no change if string is shorter than $limit
+		if(strlen($string) <= $limit) return $string;
+
+		// is $break present between $limit and the end of the string?
+		if(false !== ($breakpoint = strpos($string, $break, $limit))) {
+			if($breakpoint < strlen($string) - 1) {
+				$string = substr($string, 0, $breakpoint) . $pad;
+			}
+		}
+		return $string;
+	}
+    
+    // timeago
+	function ago($time) {
+		$periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+		$lengths = array("60","60","24","7","4.35","12","10");
+
+		$now = time();
+
+		$difference     = $now - $time;
+		$tense         = "ago";
+
+		for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+			$difference /= $lengths[$j];
+		}
+
+		$difference = round($difference);
+
+		if($difference != 1) {
+			$periods[$j].= "s";
+		}
+
+		return "$difference $periods[$j] ago";
+	}
+?>
