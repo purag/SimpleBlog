c8c734bfb63249e9e721e43f6e523fa430ce8951
Purag Moumdjian <purmou@gmail.com>
2014-03-02 17:04:48 -0800

    Create db.php
    Connect to the database and make it accessible through a class.

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+    // create class
+    class db {
+        public $pdo;
+    
+        public function connect(){
+            // database credentials
+            $server = ' ';
+            $name = ' ';
+            $username = ' ';
+            $password = ' ';
+            
+            // connect
+            $this->pdo = new PDO("mysql:host=$server;dbname=$name",$username,$password);
+        }
+    }
+    
+    // assign class to variable
+    $db = new db();
+    $db->connect();
+    $dbh = $db->pdo;
+?>
